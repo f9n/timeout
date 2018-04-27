3c77a98a5247ddb0c15639f9047626b0f38b4fdd
pleycpl <h4rvey@protonmail.com>
2018-04-27 16:40:00 +0100

    added other routes

--- main.rb
+++ main.rb
@@ -1,8 +1,26 @@
 require 'sinatra' 
+require 'json'
 
-get '/:time' do
+get '/' do
+  # Random time
+  time_amount = rand(1..10)
+  puts "Sleeping #{time_amount} seconds"
+  sleep time_amount
+  "Random Time"
+end
+
+get '/text/:time' do
   time_amount = params[:time].to_i
   puts "Sleeping #{time_amount} seconds"
   sleep time_amount
-  "Just Do It" 
+  "Just Do It"	# Return text
+end
+
+
+get '/json/:time' do
+  time_amount = params[:time].to_i
+  puts "Sleeping #{time_amount} seconds"
+  sleep time_amount
+  content_type :json
+  { foo: "Bar" }.to_json # Return json
 end
